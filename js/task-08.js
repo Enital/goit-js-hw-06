eac73ca94509a3efd51d6a22bab63c20bfdf9b4d
Enital <117684114+Enital@users.noreply.github.com>
2023-03-20 22:07:43 +0200

    Add files via upload

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formEl = event.currentTarget.elements;
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+
+    if (email === '' || password === '') {
+        alert('All fields must be filled!');
+        return;
+    }
+
+    const sendObject = {
+        email,
+        password,
+    };
+
+    console.log(sendObject);
+    event.currentTarget.reset();
+};
